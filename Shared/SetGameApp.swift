ba4dc3b296deeb89c66a7b3e8eba24b7d9aa5ed6
ahonn <ahonn95@outlook.com>
2021-12-22 23:30:05 +0800

    Initial Commit

---
+++ Shared/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  Shared
+//
+//  Created by Yuexun Jiang on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
